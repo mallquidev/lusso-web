1ff50a45e79151451818e8a64591403e976997ba
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-10-23 15:02:53 -0500

    actualizar precio

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -43,43 +43,48 @@ import cn from '../assets/c-n.jpeg'
 import db from '../assets/d-b.jpeg'
 import dn from '../assets/d-n.jpeg'
 
+import whatsap from '../assets/whatsap.svg'
 
 
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 
 const products = [
-    {
+    {   
+        id: 1,
         title: "Dress Yourself",
+        price: 45,
         subtitle: "BE UNIQUE",
         description: "Discover the Essentials for this Season.",
         img: [hondablack, hondawhite],
     },
     {
+        id: 2,
         title: "Dress Yourself",
+        price: 45,
         subtitle: "BE UNIQUE",
         description: "Discover the Essentials for this Season.",
         img: [volksblack, volksWhite],
     },
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [unon, unob],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [dosn, dosb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [tresn, tresb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [cuatron, cuatrob],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [cincon, cincob],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [seisn, seisb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [sieten],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [ochon, ochob],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [nueven, nueveb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [diezn, diezb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [once],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [docen, doceb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [trecen, treceb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [catorcen, catorceb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [quincen, quinceb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [an, ab],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [bn, bb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [cn, cb],},
-    {title: "Dress Yourself", subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [dn, db],},
+    { id: 3, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [unon, unob], },
+    { id: 4, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [dosn, dosb], },
+    { id: 5, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [tresn, tresb], },
+    { id: 6, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [cuatron, cuatrob], },
+    { id: 7, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [cincon, cincob], },
+    { id: 8, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [seisn, seisb], },
+    { id: 9, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [sieten], },
+    { id: 10, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [ochon, ochob], },
+    { id: 11, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [nueven, nueveb], },
+    { id: 12, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [diezn, diezb], },
+    { id: 13, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [once], },
+    { id: 14, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [docen, doceb], },
+    { id: 15, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [trecen, treceb], },
+    { id: 16, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [catorcen, catorceb], },
+    { id: 17, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [quincen, quinceb], },
+    { id: 18, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [an, ab], },
+    { id: 19, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [bn, bb], },
+    { id: 20, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [cn, cb], },
+    { id: 21, title: "Dress Yourself", price: 45, subtitle: "BE UNIQUE", description: "Discover the Essentials for this Season.", img: [dn, db], },
 ];
 
 const Productos = () => {
@@ -89,15 +94,26 @@ const Productos = () => {
                 {products.map((product, index) => (
                     <div key={index} className="flex flex-col justify-center items-center">
                         <div className="w-60 md:w-1/2">
-                            <ImageGallery items={product.img.map(image => ({original: image}))} showPlayButton={false} showThumbnail={false} showBullets={true} showNav={false} showFullscreenButton={false}/>
+                            <ImageGallery items={product.img.map(image => ({ original: image }))} showPlayButton={false} showThumbnail={false} showBullets={true} showNav={false} showFullscreenButton={false} />
                         </div>
                         <div className="text-center md:text-left md:w-1/2 flex flex-col items-center">
-                            <p className="text-[#797979] md:text-3xl">{product.subtitle}</p>
                             <p className="text-3xl font-bold">{product.title}</p>
+                            <p className="text-[#797979] md:text-3xl">{product.subtitle}</p>
+                            <div className="flex items-end items-center">
+                                <span className="relative text-lg font-semibold text-[#11181c]">${product.price}</span>
+                                <span className="relative line-through font-semibold text-[#21A0A] ml-3">$56</span>
+                                
+                            </div>
                             <p className="mt-4 text-sm text-center">{product.description}</p>
-                            <button className="border mt-4 border-b-black bg-black text-sm font-bold px-4 py-2 text-white border-b-2 transition-colors duration-300 ease-in-out hover:bg-black hover:text-white">
-                                SHOW NOW
-                            </button>
+                            
+                            <a
+                                target='_blank' rel="noopener noreferrer"
+                                className="flex mt-4 k bg-black text-sm font-bold px-4 py-2 text-white border-b-2 transition-colors duration-300 ease-in-out hover:bg-[#2e2e2e] hover:text-white"
+                                href={`https://wa.me/+5217292818795?text=Hola,%20me%20gustaría%20recibir%20más%20información%20sobre%20${encodeURIComponent(product.title)}`}>
+                                <img src={whatsap} alt="WhatsApp" className="w-6 h-6 mr-2" />
+                                Reservar
+                            </a>
+                            
                         </div>
                     </div>
                 ))}
